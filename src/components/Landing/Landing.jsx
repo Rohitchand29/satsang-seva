4d477abb84c417e890143cf5a81d757c82c82c77
rohit chand <rohitchand010904@gmail.com>
2024-04-05 23:09:29 +0530

    landing page fixes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,62 +2,8 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>
-    <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
-      <p>Lanmdjngsdflkfds</p>
+    <div className='pt-14'>
+      <p>Landing page</p>
     </div>
   )
 }
