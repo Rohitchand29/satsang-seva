7609b083902bd22f786d0cd0010eb06e93caed97
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-04-08 21:04:29 +0530

    city fetch

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import { CiSearch } from "react-icons/ci";
 import Dropdown from "../Dropdown/Dropdown";
 import { useGeoLocation } from "../../Hooks";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Navbar = () => {
   const { location, error, refresh } = useGeoLocation();
+  const [city, setCity] = useState("");
   console.log({ location, error, refresh })
+
+  const getCityName = async () => {
+    console.log(process.env.REACT_APP_OPENWEATHER_API_KEY)
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=28.950794063078728&lon=77.06840059488145&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+
+      const data = await response.data;
+      console.log(data.name)
+      setCity(data.name)
+  }
+  useEffect(()=>{
+    if (location) {
+      getCityName();
+    }
+  })
+
   return (
     <div>
       <nav className="bg-secondary  text-text w-full py-4 px-8 z-10 fixed ">
@@ -19,7 +37,7 @@ const Navbar = () => {
                 <input
                   type="search"
                   placeholder="Search Bar"
-                  className="  flex text-center outline-none w-[600px]  py-1 text-black"
+                  className="  flex text-start outline-none w-[600px] py-1 text-black"
                 />
                 <CiSearch className="" />
               </div>
@@ -29,10 +47,7 @@ const Navbar = () => {
               ()=>{
               }
             } className="rounded hover:shadow hover:duration-300 duration-300 cursor-pointer hover:bg-accent ml-20 font-serif">
-              {(location)?<div>
-                lat: {location.latitude}
-                lon: {location.longitude}
-              </div>:<div>location</div>}
+              {(city)?<>{city}</>:<>Location</>}
             </button>
             <button className="rounded hover:shadow hover:duration-300 duration-300 cursor-pointer hover:bg-accent ml-20 font-serif">
               Login
