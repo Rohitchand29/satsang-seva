3afb54d7fb20d9fb7728bb5dcc6d3fee5b5b2b5e
Anurag Daksh <anuragdaksh77777@gmail.com>
2024-04-09 02:03:01 +0530

    nextify

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import routeLinks from "./routeLinks";
+
+export {
+  routeLinks
+}
---
+++ hooks/useGeoLocation.js
@@ -0,0 +1,50 @@
+'use client'
+import { useState } from 'react';
+
+export default function useGeoLocation() {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  const refresh = () => {
+    setLocation(null);
+    setError(null);
+  };
+
+  // useEffect(() => {
+  //   if (!navigator.geolocation) {
+  //     setError('Geolocation is not supported by this browser.');
+  //     return;
+  //   }
+
+  //   function handleSuccess(position) {
+  //     const { latitude, longitude } = position.coords;
+  //     setLocation({ latitude, longitude });
+  //   }
+
+  //   function handleError(error) {
+  //     setError(error.message);
+  //   }
+
+  //   navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  // }, []);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setError('Geolocation is not supported by this browser.');
+      return;
+    }
+
+    function handleSuccess(position) {
+      const { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude });
+    }
+
+    function handleError(error) {
+      setError(error.message);
+    }
+
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+  }
+
+  return { location, getLocation, error, refresh };
+}
